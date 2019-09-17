73ee49f213033f89bab42413bcf5f225778e983a
Kadzup <dimonstefurak@gmail.com>
2019-09-17 22:34:38 +0300

    Update Country.java

--- src/Country.java
+++ src/Country.java
@@ -2,4 +2,10 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-public class Country {}
+public class Country {
+    public static String name;
+
+    public static String buildShortcut(){
+            return name[0]+name[1]+name[3];
+    }
+}
